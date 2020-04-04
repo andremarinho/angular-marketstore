47fc6084514bff1053596f906bc9e157850774ef
andremarinho <andre.bmarinho@hotmail.com>
2020-04-04 07:06:03 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-marketstore-uuacuw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-marketstore-uuacuw)
---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'Bolsa Sintetica Feminina',
+    price: 199,
+    description: 'Marrom com alça lateral'
+  },
+  {
+    name: 'Bolsa de Couro Feminina',
+    price: 750,
+    description: 'Courp destonado'
+  },
+  {
+    name: 'Carteira Feminina',
+    price: 299,
+    description: ''
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
